03f061997350f4ff9c10ec961368448e51d879a7
Luiz Roberto <luizmala9@hotmail.com>
2025-06-26 16:45:55 -0300

    Spring Boot project

---
+++ src/test/java/com/luizroberto/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizroberto.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
