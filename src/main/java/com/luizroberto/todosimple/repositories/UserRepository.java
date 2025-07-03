eb66b6c6f1a2da0e7802e6121a963d2f024d2dac
Luiz Roberto <luizmala9@hotmail.com>
2025-07-03 17:26:17 -0300

    Add lombok

--- src/main/java/com/luizroberto/todosimple/repositories/UserRepository.java
+++ src/main/java/com/luizroberto/todosimple/repositories/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.luizroberto.todosimple.models.User;
-import java.util.List;
+
 
 
 @Repository
