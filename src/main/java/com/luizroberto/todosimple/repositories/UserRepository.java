bc0f5777e9d839389774319198a8d0e485e9404c
Luiz Roberto <luizmala9@hotmail.com>
2025-07-28 18:24:03 -0300

    Create UserDetailServiceImp nad JWTUtil

--- src/main/java/com/luizroberto/todosimple/repositories/UserRepository.java
+++ src/main/java/com/luizroberto/todosimple/repositories/UserRepository.java
@@ -3,12 +3,19 @@ package com.luizroberto.todosimple.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 import com.luizroberto.todosimple.models.User;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     
+     
+    User findByUsername(String username);
    
 }
